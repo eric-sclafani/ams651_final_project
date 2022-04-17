3bd59598237272c67542358d2f78cf6e98609a57
eric-sclafani <eric.sclafani321@gmail.com>
2022-04-17 12:35:20 -0400

    finished 1st iteration of bayes

--- README.md
+++ README.md
@@ -12,10 +12,8 @@
 `So far, I think I will either use SVM and/or Naive Bayes (easiest w/ BoW)`
 
 
-
-
 ## Useful Links for learning SVM, sklearn, Naive Bayes, sentiment analysis. 
-**NOT FOR COPYING, ONLY LEARNING FROM**
+**NOT FOR COPYING, ONLY LEARNING FROM. I TRIED TO FIND LINKS WITH MINIMAL/NO CODE**
 
 https://towardsdatascience.com/a-brief-introduction-to-support-vector-machine-adf0f103a80f
 
@@ -28,3 +26,9 @@ https://en.wikipedia.org/wiki/Tf%E2%80%93idf
 https://scikit-learn.org/stable/tutorial/machine_learning_map/
 
 https://www.dataschool.io/comparing-supervised-learning-algorithms/
+
+https://www.analyticsvidhya.com/blog/2017/09/naive-bayes-explained/
+
+https://medium.com/@kcatstack/sentiment-analysis-naive-bayes-classifier-from-scratch-part-1-theory-4949115ba13
+
+https://towardsdatascience.com/a-hitchhikers-guide-to-sentiment-analysis-using-naive-bayes-classifier-b921c0fb694
--- text_preprocessor.py
+++ text_preprocessor.py
@@ -1,28 +1,48 @@
-import numpy as np
+from nltk.corpus import stopwords
 import pandas as pd
-import re
+from re import sub, findall
 
-unprocessed_df = pd.read_csv("ice_cream_reviews.csv") # preserve original df
+# words that dont have any sentiment
+s_words = stopwords.words("english")
+s_words.extend(["ive", "much","ice", "cream", "im", "dont", "try", "people", "cookie", "ill", "get"]) # add more stopwords to list
 
-def process_df(df):
-    
-    # drop useless columns
-    df.drop(["author", "date", "helpful_yes", "helpful_no"], inplace=True, axis=1)
-    
-    # drop messed up rows 
-    # some are incorrectly formatted. The ones that dont start with a key are a part of the review above it.
-    # would like to fix all of them, but there's over 8k entries.
-    df = df[df["key"].str.contains(r"\d+_bj", regex=True)] 
-    
-    # convert ratings: rating < 3 == 0 (negative), >= 3 == 1 (positive)
-    ratings = pd.Series([0 if n < 3 else 1 for n in df["stars"]])
 
-    # combine title and text columns (make sense because titles can have sentiment words in them)
-    text = df["title"] + " " + df["text"]
+def textcleaner(text):
+    # clean text and remove stop words (useless words with no meaning)
+    text = sub(r"[^\w\s]", "", text).lower().strip().split()
+    print(text)
+    return " ".join([word for word in text if word not in s_words])
+
+def tokenizer(text):
+    return findall(r"\w+", text.lower().strip())
+
+def process_df(fname):
+
+    df = pd.read_csv(fname)
+    df.drop(["author", "date", "helpful_yes", "helpful_no"], inplace=True, axis=1) # drop useless columns
     
-    # putting it all together
-    cleaned_df = pd.DataFrame({"rating": ratings,
-                               "review": text
-                              })
+    sentiment = pd.Series([0 if n <=2 else 1 for n in df["stars"]]) # convert ratings: rating < 3 == 0 (negative), >= 3 == 1 (positive)
+    
+    conversion = pd.DataFrame({"stars": df["stars"], # debugging purposes:shows correspondance between stars (1-5) and sentiment ratings (0,1)
+                               "sentiment": sentiment})
+    # conversion.to_csv("conversion_ratio.csv")
+
+    text = df["title"] + " " + df["text"] # combine title and text columns into a Series (makes sense because titles can have sentiment words in them)
+    
+    cleaned_df = pd.DataFrame({"sentiment": sentiment, # combine into one df
+                               "review": text})
+    cleaned_df.dropna(inplace=True)
+    
+    cleaned_df = pd.DataFrame({"sentiment": cleaned_df["sentiment"],
+                               "review":list(map(lambda x: textcleaner(x), list(cleaned_df["review"])))}) # clean each text review and put everything together
     
     return cleaned_df
+
+
+
+# DATAFRAME DEBUGGING CODE
+
+# df = process_df("ice_cream_reviews.csv")
+# print(df.head(10))
+# with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+#     print(df)
