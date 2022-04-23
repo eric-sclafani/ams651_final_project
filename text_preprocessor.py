a6d31050b90aad2234b5fa9e2745b06991f66abd
eric-sclafani <eric.sclafani321@gmail.com>
2022-04-23 09:47:13 -0400

    documented bayes, instaniated SVM

--- text_preprocessor.py
+++ text_preprocessor.py
@@ -4,32 +4,58 @@ from re import sub, findall
 
 # words that dont have any sentiment
 s_words = stopwords.words("english")
-s_words.extend(["ive", "much","ice", "cream", "im", "dont", "try", "people", "cookie", "ill", "get"]) # add more stopwords to list
+s_words.extend(["ive", "much","ice", "cream", "im", "dont", 
+                "try", "people", "cookie", "ill", "get", ]) 
 
 
 def textcleaner(text):
-    # clean text and remove stop words (useless words with no meaning)
+    """
+    Normalizes text (removes punctuation and stopwords, strips, lowercase)
+    
+    Args:
+        text (str): string to be cleaned
+    Returns:
+        Cleaned string
+    
+    """
     text = sub(r"[^\w\s]", "", text).lower().strip().split()
-    print(text)
     return " ".join([word for word in text if word not in s_words])
 
 def tokenizer(text):
+    """
+    Tokenizes a string
+    
+    Args:
+        text (str): string to be tokenized
+    Returns:
+        Tokenized string
+    """
     return findall(r"\w+", text.lower().strip())
 
-def process_df(fname):
+def process_df(fname, to_drop):
+    """
+    Processes a csv to be used in a classifier:
+        - reads in a csv as a dataframe
+        - drops useless columns
+        - converts star ratings into 0s and 1s
+        - combines everything back into one dataframe and drops na values
+        - cleans each text review
+    
+    Args:
+        fname (str): file to be read
+        to_drop (list): list of columns to drop
+    Returns:
+        Cleaned dataframe
+    """
 
     df = pd.read_csv(fname)
-    df.drop(["author", "date", "helpful_yes", "helpful_no"], inplace=True, axis=1) # drop useless columns
+    df.drop(to_drop, inplace=True, axis=1)
     
     sentiment = pd.Series([0 if n <=2 else 1 for n in df["stars"]]) # convert ratings: rating < 3 == 0 (negative), >= 3 == 1 (positive)
-    
-    conversion = pd.DataFrame({"stars": df["stars"], # debugging purposes:shows correspondance between stars (1-5) and sentiment ratings (0,1)
-                               "sentiment": sentiment})
-    # conversion.to_csv("conversion_ratio.csv")
 
     text = df["title"] + " " + df["text"] # combine title and text columns into a Series (makes sense because titles can have sentiment words in them)
     
-    cleaned_df = pd.DataFrame({"sentiment": sentiment, # combine into one df
+    cleaned_df = pd.DataFrame({"sentiment": sentiment, 
                                "review": text})
     cleaned_df.dropna(inplace=True)
     
@@ -39,10 +65,3 @@ def process_df(fname):
     return cleaned_df
 
 
-
-# DATAFRAME DEBUGGING CODE
-
-# df = process_df("ice_cream_reviews.csv")
-# print(df.head(10))
-# with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-#     print(df)
